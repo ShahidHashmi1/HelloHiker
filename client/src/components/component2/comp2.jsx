f935cda72eb1cac66852cd845fa2e491893304e9
ShahidHashmi1 <hashmi.pgh@gmail.com>
2022-12-21 18:32:54 -0500

    new packages

--- client/src/components/component2/comp2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const comp2 = () => {
-  return (
-    <div>comp2</div>
-  )
-}
-
-export default comp2
--- client/src/components/component3/comp3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const comp3 = () => {
-  return (
-    <div>comp3</div>
-  )
-}
-
-export default comp3
