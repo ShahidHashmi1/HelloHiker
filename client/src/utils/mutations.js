2e4e79d8d524d5bfb03c99cc70aa2c5d358825b2
Nick Mason <nickmason372@yahoo.com>
2022-12-22 19:11:47 -0500

    mutations added

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,47 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_TRAIL = gql`
+    mutation addTrail($profileId: ID!){
+        addTrail(profileId: $profileId){
+            token
+            user {
+                _id
+            }
+        }
+    }
+    `;
+
+export const REMOVE_TRAIL =gql`
+mutation removeTrail($profileId: ID!){
+    removeTrail(profileId: $profileId){
+        token
+        user {
+            _id
+        }
+    }
+}
+`;
