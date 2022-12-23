79506f67913674498d886ac6dccc06c5c211e30b
ShahidHashmi1 <hashmi.pgh@gmail.com>
2022-12-22 19:12:01 -0500

    update to component structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import Header from '../components/Header/Header';
+import Nav from '../components/Nav/Nav';
+
+const Home = () => {
+
+
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+
+}
+
+export default Home
