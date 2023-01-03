9e7f4d2bafd99befe488afd0cbb765608c723dcf
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2023-01-02 20:56:48 -0500

    users page

---
+++ client/src/components/Users/userData.jsx
@@ -0,0 +1,28 @@
+import user1 from '../../Images/testimonialPics/test4.jpg';
+import user2 from '../../Images/testimonialPics/test5.jpg';
+import user3 from '../../Images/testimonialPics/test3.webp'
+
+   const userData = [
+        {
+            name: 'Adam S.',
+            description: 'Hiking is my passion. The best part is being out in nature. I have hiked over 10 trails in the past year!',
+            link: 'http://user1.com',
+            image: user1
+        },
+        {
+            name: 'Nick M.',
+            description: 'My favorite trail is Bryce Canyon national park.',
+            link: 'http://www.user2.com',
+            image: user2
+        },
+        {
+            name: 'Kelly R.',
+            description: 'Happy hiker since 2001!',
+            link: 'http://www.user3.com',
+            image: user3
+        },
+        
+    ]
+  
+
+export default userData 
